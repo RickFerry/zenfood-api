36fa1864a4443b2b2a5a5b6234a1eaeaeb03c61a
Ricardo <rick_ferry1@hotmail.com>
2025-02-15 00:57:41 -0300

    Initial commit

---
+++ src/test/java/com/ferry/zenfoodapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferry.zenfoodapi;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+   @Test
+    public void contextLoads() {
+        Application.main(new String[] {});
+    }
+}
