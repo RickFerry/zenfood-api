6ba364bd35481a368200095a5c0af52094a036f8
RickFerry <rick_ferry1@hotmail.com>
2025-02-15 15:58:27 -0300

    Formatando sql, importe inicial de dados e novas entidades

---
+++ src/main/java/com/ferry/zenfoodapi/domain/model/Restaurante.java
@@ -0,0 +1,29 @@
+package com.ferry.zenfoodapi.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "restaurantes")
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private BigDecimal taxaFrete;
+    private Boolean ativo;
+    private Boolean aberto;
+    private LocalDateTime dataCadastro;
+    private LocalDateTime dataAtualizacao;
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into cozinhas (nome) values ('Brasileira');
+insert into cozinhas (nome) values ('Japonesa');
+insert into cozinhas (nome) values ('Italiana');
+insert into cozinhas (nome) values ('Chinesa');
+insert into cozinhas (nome) values ('Mexicana');
