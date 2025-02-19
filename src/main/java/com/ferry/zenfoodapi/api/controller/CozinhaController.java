1f8f681923120411500a0816faded6b07bb71e6a
Ricardo <rick_ferry1@hotmail.com>
2025-02-18 21:26:32 -0300

    CRUD para recurso de Cozinha

--- src/main/java/com/ferry/zenfoodapi/api/controller/CozinhaController.java
+++ src/main/java/com/ferry/zenfoodapi/api/controller/CozinhaController.java
@@ -6,9 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +18,42 @@ public class CozinhaController {
     @GetMapping
     public ResponseEntity<Page<Cozinha>> listar(Pageable pageable) {
         return ResponseEntity.ok(cozinhaService.listar(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(cozinhaService.buscar(id));
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Cozinha> adicionar(@RequestBody Cozinha cozinha, UriComponentsBuilder uriBuilder) {
+        Cozinha cozinhaSalva = cozinhaService.salvar(cozinha);
+        return ResponseEntity.created(uriBuilder.path("/{id}")
+                .buildAndExpand(cozinhaSalva.getId())
+                .toUri()).body(cozinhaSalva);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long id, @RequestBody Cozinha cozinha) {
+        try {
+            Cozinha cozinhaAtualizada = cozinhaService.atualizar(id, cozinha);
+            return ResponseEntity.ok(cozinhaAtualizada);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable Long id) {
+        try {
+            cozinhaService.remover(id);
+            return ResponseEntity.noContent().build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/ferry/zenfoodapi/api/service/CozinhaService.java
+++ src/main/java/com/ferry/zenfoodapi/api/service/CozinhaService.java
@@ -3,6 +3,7 @@ package com.ferry.zenfoodapi.api.service;
 import com.ferry.zenfoodapi.domain.model.Cozinha;
 import com.ferry.zenfoodapi.domain.repository.CozinhaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,31 @@ public class CozinhaService {
     @Transactional(readOnly = true)
     public Page<Cozinha> listar(Pageable pageable) {
         return cozinhaRepository.findAll(pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public Cozinha buscar(Long id) {
+        return getOredElseThrow(id);
+    }
+
+    @Transactional
+    public Cozinha salvar(Cozinha cozinha) {
+        return cozinhaRepository.save(cozinha);
+    }
+
+    @Transactional
+    public Cozinha atualizar(Long id, Cozinha cozinha) {
+        Cozinha cozinhaAtual = getOredElseThrow(id);
+        BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+        return cozinhaRepository.save(cozinhaAtual);
+    }
+
+    @Transactional
+    public void remover(Long id) {
+        cozinhaRepository.delete(getOredElseThrow(id));
+    }
+
+    private Cozinha getOredElseThrow(Long id) {
+        return cozinhaRepository.findById(id).orElseThrow(() -> new RuntimeException("Cozinha não encontrada"));
     }
 }
